7de9f3f171b042a0bbf4fea77a34acb9f8f8f0e2
peterdavidfagan <peterdavidfagan@gmail.com>
2024-02-13 09:08:24 +0000

    minor clean of single image policy

--- src/tutorials/policy_deployment/panda_policy_deployment_demos/single_image_pose_policy.py
+++ src/tutorials/policy_deployment/panda_policy_deployment_demos/single_image_pose_policy.py
@@ -9,34 +9,12 @@ from moveit.policies import Policy
 from geometry_msgs.msg import PoseStamped
 from moveit_msgs.srv import ServoCommandType
 from std_srvs.srv import SetBool
+from ament_index_python.packages import get_package_share_directory
+
+import onnx
+import onnxruntime as ort
 
 from panda_policy_deployment_demos.panda_policy_deployment_demos_parameters import policy as params
-
-#import jax
-#import flax
-#import flax.linen as nn
-
-
-#class CNN(nn.Module):
-#  """A simple CNN model."""
-
-#  @nn.compact
-#  def __call__(self, x):
-#    x = nn.Conv(features=32, kernel_size=(3, 3))(x)
-#    x = nn.relu(x)
-#    x = nn.avg_pool(x, window_shape=(2, 2), strides=(2, 2))
-#    x = nn.Conv(features=64, kernel_size=(3, 3))(x)
-#    x = nn.relu(x)
-#    x = nn.avg_pool(x, window_shape=(2, 2), strides=(2, 2))
-#    x = x.reshape((x.shape[0], -1))  # flatten
-#    x = nn.Dense(features=256)(x)
-#    x = nn.relu(x)
-#    x = nn.Dense(features=7)(x)
-
-    # potential to apply constraints to network outputs to 
-    # ensure poses are within the robot workspace
-
-#    return x
 
 class SingleImagePolicy(Policy):
     """Dummy policy for testing."""
@@ -48,10 +26,10 @@ class SingleImagePolicy(Policy):
         # use CvBridge to convert sensor_msgs/Image to numpy array
         self.cv_bridge = CvBridge()
         
-        # Dummy neural network
- #       self.cnn = CNN()
- #       self.cnn_params = self.cnn.init(jax.random.PRNGKey(0), jax.numpy.ones((360, 640, 3)))
-        
+        # start onnx inference session
+        #model_path = get_package_share_directory("panda_policy_deployment_demos") + "/models/your_model.onnx"
+        #self.cnn = ort.InferenceSession(model_path)
+
         # initialize servo service client
         self.servo_command_client = self.create_client(ServoCommandType, "/servo_node/switch_command_type")
         while not self.servo_command_client.wait_for_service(timeout_sec=1.0):
@@ -72,18 +50,13 @@ class SingleImagePolicy(Policy):
         else:
             self.get_logger().error("Failed to set servo command type: %s" % command_type)
 
-
     def forward(self, sensor_msg):
         """Forward pass of the policy."""
         if self._is_active:
             # convert image data to jax array
             cv_img = self.cv_bridge.imgmsg_to_cv2(sensor_msg, desired_encoding="rgb8")
-  #          image = jax.numpy.array(cv_img)
-  #          self._logger.debug("Image shape: {}".format(image.shape))
-
-            # add code to perform forward pass through your neural network here
-  #          dummy_action = self.cnn.apply(self.cnn_params, image)
-  #          self._logger.debug("Dummy action shape: {}".format(dummy_action.shape))
+            # perform inference with onnx model
+            #prediction = self.cnn.run(None, {"input": cv_img})
 
             # For this demo, we will just publish random target poses
             target = PoseStamped()
@@ -106,13 +79,10 @@ def main():
     logger = rclpy.logging.get_logger("policy_deployment")
     
     policy = SingleImagePolicy()
-    
-    # basic policy configuration
     policy.active = True
-    policy.set_servo_command_type(ServoCommandType.Request.POSE) # set servo command type to pose
+    policy.set_servo_command_type(ServoCommandType.Request.POSE) 
     
     rclpy.spin(policy)
-
     policy.destroy_node()
     rclpy.shutdown()
 
