9b33859c4c6f99baa23e61cfc36e1e12ae797861
peterdavidfagan <peterdavidfagan@gmail.com>
2024-02-22 18:27:18 +0000

    include source of install setup bash

--- ros_workspace_setup.sh
+++ ros_workspace_setup.sh
@@ -9,9 +9,11 @@ cd ./src/motion_planning
 for repo in moveit2/moveit2.repos $(f="moveit2/moveit2_$ROS_DISTRO.repos"; test -r $f && echo $f); do vcs import < "$repo"; done
 
 # install rosdep dependencies
-cd ../../
+cd ..
 rosdep install --from-paths . --ignore-src --rosdistro rolling -r -y
+cd ..
 
 # build the workspace
 source /opt/ros/rolling/setup.bash
 colcon build --mixin release
+source ./install/setup.bash
